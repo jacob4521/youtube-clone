2fd6674fc875948ab0616eb10c66655d990ca8f9
Zakib <zakib6354@gmail.com>
2025-11-06 10:56:52 +0530

    Create the routes for the Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/video/:categoryId/:videoId' element={<Video />} />
+      </Routes>
     </div>
   )
 }
