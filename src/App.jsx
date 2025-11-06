ede70dac4bb50dfffe411049bddca3a7902ef32e
Zakib <zakib6354@gmail.com>
2025-11-06 11:33:10 +0530

    Cerate the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import { Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Video from './pages/Video/Video'
 
 const App = () => {
   return (
