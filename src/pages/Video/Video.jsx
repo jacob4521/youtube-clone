c7c55cad5fa8cb1fadef6a3a669b1119a3d181dd
Zakib <zakib6354@gmail.com>
2025-11-11 14:48:51 +0530

    Create Recommended component

--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import './Video';
+import React from 'react';
+import './Video.css';
 import PlayVideo from '../../components/PlayVideo/PlayVideo';
+import Recmommended from '../../components/Recommended/Recommended';
 
 const Video = () => {
   return (
     <div className='play-container'>
       <PlayVideo />
+      <Recmommended />
     </div>
   )
 }
