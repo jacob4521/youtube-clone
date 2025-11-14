3fbde2a8d1ca49535cf7707b39a077861ac2f23c
Zakib <zakib6354@gmail.com>
2025-11-14 16:19:18 +0530

    Display feed video cards from the api,

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -16,36 +16,36 @@ import megan from '../../assets/megan.png'
 import cameron from '../../assets/cameron.png'
 
 
-const Sidebar = ({sidebar, category, setCategory}) => {
+const Sidebar = ({ sidebar, category, setCategory }) => {
   return (
-    <div className={`sidebar ${sidebar? "":"small-sidebar"}`}>
+    <div className={`sidebar ${sidebar ? "" : "small-sidebar"}`}>
       <div className="shortcut-links">
-        <div className={`side-link ${category===0 ? "active" : ""}`}>
-          <img src={home} alt="" onClick={() => {setCategory(0)}} /><p>Home</p>
+        <div onClick={() => { setCategory(0) }} className={`side-link ${category === 0 ? "active" : ""}`}>
+          <img src={home} alt="" /><p>Home</p>
         </div>
-        <div className={`side-link ${category===20 ? "active" : ""}`}>
-          <img src={game_icon} alt="" onClick={() => {setCategory(20)}} /><p>Gaming</p>
+        <div onClick={() => { setCategory(20) }} className={`side-link ${category === 20 ? "active" : ""}`}>
+          <img src={game_icon} alt="" /><p>Gaming</p>
         </div>
-        <div className={`side-link ${category===2 ? "active" : ""}`}>
-          <img src={automobiles} alt="" onClick={() => {setCategory(2)}} /><p>Automobiles</p>
+        <div onClick={() => { setCategory(2) }} className={`side-link ${category === 2 ? "active" : ""}`}>
+          <img src={automobiles} alt="" /><p>Automobiles</p>
         </div>
-        <div className={`side-link ${category===17 ? "active" : ""}`}>
-          <img src={sports} alt="" onClick={() => {setCategory(17)}} /><p>Sports</p>
+        <div onClick={() => { setCategory(17) }} className={`side-link ${category === 17 ? "active" : ""}`}>
+          <img src={sports} alt="" /><p>Sports</p>
         </div>
-        <div className={`side-link ${category===24 ? "active" : ""}`}>
-          <img src={entertainment} alt="" onClick={() => {setCategory(24)}} /><p>Entertainment</p>
+        <div onClick={() => { setCategory(24) }} className={`side-link ${category === 24 ? "active" : ""}`}>
+          <img src={entertainment} alt="" /><p>Entertainment</p>
         </div>
-        <div className={`side-link ${category===28 ? "active" : ""}`}>
-          <img src={tech} alt="" onClick={() => {setCategory(28)}} /><p>Technology</p>
+        <div onClick={() => { setCategory(28) }} className={`side-link ${category === 28 ? "active" : ""}`}>
+          <img src={tech} alt="" /><p>Technology</p>
         </div>
-        <div className={`side-link ${category===10 ? "active" : ""}`}>
-          <img src={music} alt="" onClick={() => {setCategory(10)}} /><p>Music</p>
+        <div onClick={() => { setCategory(10) }} className={`side-link ${category === 10 ? "active" : ""}`}>
+          <img src={music} alt="" /><p>Music</p>
         </div>
-        <div className={`side-link ${category===22 ? "active" : ""}`}>
-          <img src={blogs} alt="" onClick={() => {setCategory(22)}} /><p>Blogs</p>
+        <div onClick={() => { setCategory(22) }} className={`side-link ${category === 22 ? "active" : ""}`}>
+          <img src={blogs} alt="" /><p>Blogs</p>
         </div>
-        <div className={`side-link ${category===25 ? "active" : ""}`}>
-          <img src={news} alt="" onClick={() => {setCategory(25)}} /><p>News</p>
+        <div onClick={() => { setCategory(25) }} className={`side-link ${category === 25 ? "active" : ""}`}>
+          <img src={news} alt="" /><p>News</p>
         </div>
         <hr />
       </div>
--- src/data.jsx
+++ src/data.jsx
@@ -1,3 +1,11 @@
-const API_KEY = "AIzaSyBXX4GNWvHWtkNz67lXWAwJUWWWNNViSuU";
+export const API_KEY = "AIzaSyBXX4GNWvHWtkNz67lXWAwJUWWWNNViSuU";
 
-export default API_KEY;
+export const value_converter = ( value ) => {
+  if (value >= 1000000) {
+    return Math.floor(value/1000000) + "M";
+  } else if (value >= 1000) {
+    return Math.floor(value/1000) + "K";
+  } else {
+    return value;
+  }
+};
