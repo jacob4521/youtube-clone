c36f023a3daaf828872fd6a53072e069f3650b9c
Zakib <zakib6354@gmail.com>
2026-05-16 14:59:55 +0530

    Moving the changed new API key to .env file

--- src/data.jsx
+++ src/data.jsx
@@ -1,10 +1,10 @@
-export const API_KEY = "AIzaSyBXX4GNWvHWtkNz67lXWAwJUWWWNNViSuU";
+export const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
 
-export const value_converter = ( value ) => {
+export const value_converter = (value) => {
   if (value >= 1000000) {
-    return Math.floor(value/1000000) + "M";
+    return Math.floor(value / 1000000) + "M";
   } else if (value >= 1000) {
-    return Math.floor(value/1000) + "K";
+    return Math.floor(value / 1000) + "K";
   } else {
     return value;
   }
